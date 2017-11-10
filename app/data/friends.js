a72544bd4bbd7d8aa3f559064ff3334b9f006324
jachaffee <jacob.chaffee@gmail.com>
2017-11-10 10:21:55 -0600

    added dummy data profiles

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var userProfiles = [
 	{
-		name:
-		photo:
-		answers:
+		name: "Andy Dwyer",
+		photo: "http://cdn1.theodysseyonline.com/files/2016/02/11/635908222768920676-1621582280_andy%20dwyer.jpg",
+		answers: [4,3,5,1,4,2,1,5,5,1]
 	},
 	{
-		name:
-		photo:
-		answers:
+		name: "Philip J. Fry",
+		photo: "https://static1.squarespace.com/static/51a655b0e4b0449f53c1e503/5522c8f1e4b091cad96f0a03/581f213115d5dbd666df5268/1478435128348/Screen+Shot+2016-11-06+at+7.23.44+AM.png?format=500w",
+		answers: [1,3,5,4,5,2,1,3,2,1]
 	},
 	{
-		name:
-		photo:
-		answers:
+		name: "Bob Belcher",
+		photo: "http://vignette2.wikia.nocookie.net/bobsburgers/images/7/7f/Bob-2.jpg/revision/latest?cb=20150623151749&path-prefix=de",
+		answers: [2,4,3,3,1,2,3,2,3,4]
 	},
 	{
-		name:
-		photo:
-		answers:
+		name: "Dick Cheney",
+		photo: "http://rightweb.irc-online.org/wp-content/uploads/2016/03/richard-dick-cheney1.jpg",
+		answers: [1,1,1,5,2,5,4,5,4,2]
 	}
 
 ];
