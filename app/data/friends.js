8f335a3ec011bf477ca48bf83f982b988ddc1f0a
jachaffee <jacob.chaffee@gmail.com>
2017-11-10 16:55:06 -0600

    functioning app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var userProfiles = [
 	{
 		name: "Andy Dwyer",
-		photo: "http://cdn1.theodysseyonline.com/files/2016/02/11/635908222768920676-1621582280_andy%20dwyer.jpg",
+		photo: "<img src=http://cdn1.theodysseyonline.com/files/2016/02/11/635908222768920676-1621582280_andy%20dwyer.jpg>",
 		answers: [4,3,5,1,4,2,1,5,5,1]
 	},
 	{
 		name: "Philip J. Fry",
-		photo: "https://static1.squarespace.com/static/51a655b0e4b0449f53c1e503/5522c8f1e4b091cad96f0a03/581f213115d5dbd666df5268/1478435128348/Screen+Shot+2016-11-06+at+7.23.44+AM.png?format=500w",
+		photo: "<img src=https://static1.squarespace.com/static/51a655b0e4b0449f53c1e503/5522c8f1e4b091cad96f0a03/581f213115d5dbd666df5268/1478435128348/Screen+Shot+2016-11-06+at+7.23.44+AM.png?format=500w>",
 		answers: [1,3,5,4,5,2,1,3,2,1]
 	},
 	{
 		name: "Bob Belcher",
-		photo: "http://vignette2.wikia.nocookie.net/bobsburgers/images/7/7f/Bob-2.jpg/revision/latest?cb=20150623151749&path-prefix=de",
+		photo: "<img src=http://vignette2.wikia.nocookie.net/bobsburgers/images/7/7f/Bob-2.jpg/revision/latest?cb=20150623151749&path-prefix=de>",
 		answers: [2,4,3,3,1,2,3,2,3,4]
 	},
 	{
 		name: "Dick Cheney",
-		photo: "http://rightweb.irc-online.org/wp-content/uploads/2016/03/richard-dick-cheney1.jpg",
+		photo: "<img src=http://rightweb.irc-online.org/wp-content/uploads/2016/03/richard-dick-cheney1.jpg>",
 		answers: [1,1,1,5,2,5,4,5,4,2]
 	}
 
